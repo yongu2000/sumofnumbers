a2081fe5554ec5fdd0847d3ba6a98306393f5b77
yongu2000 <yongu200@hawaii.edu>
2021-02-07 08:51:16 +0900

    Finished Exercise

---
+++ sumofnumbers.js
@@ -0,0 +1,37 @@
+const testNumbs = [1, 2, 3, 4];
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+}
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+}
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, num) {
+    return memo + num;
+  }, 0);
+}
+
+console.log(sumFor(testNumbs));
+console.log(sumWhile(testNumbs));
+console.log(sumRecursion(testNumbs));
+console.log(sumTheSimpleWay(testNumbs));
